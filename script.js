aa58f6c9839c4718503443200408b521f53ca166
JuniorHsG <hectorsotelogomez@gmail.com>
2025-05-08 22:53:44 -0600

    fix: corregir ubicacion y portafolio despliega modal

--- script.js
+++ script.js
@@ -39,3 +39,20 @@ function efectoHabilidades(){
 window.onscroll = function(){
     efectoHabilidades();
 } 
+
+// Función para abrir el modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+// Función para cerrar el modal
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+// Cerrar el modal si se hace clic fuera del contenido
+window.onclick = function(event) {
+    if (event.target == document.getElementById("myModal")) {
+        closeModal();
+    }
+}
